b628ed42ae27fcb97d5cdd4632b944510b6f5629
KonstantinVinogorodskiy <32172492+KonstantinVinogorodskiy@users.noreply.github.com>
2018-02-20 18:09:51 +0300

    Add files via upload

---
+++ data/aligner/aligner.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[101]:
+
+
+deutsch_toalign = open('german_translated.txt', encoding='utf-8', errors='ignore').read()
+yiddish_toalign = open('yiddish_segmented.txt', encoding='utf-8', errors='ignore').read()
+import re
+deutsch_preprocessed = re.sub('\.', ' .', deutsch_toalign)
+deutsch_splitted = deutsch_preprocessed.split('\n') 
+yiddish_splitted = yiddish_toalign.split('\n') 
+aligned = open('aligned_corpora.txt', 'w', encoding='utf-8', errors='ignore') 
+for x, y in zip(yiddish_splitted, deutsch_splitted):
+    if len(x) and len(y) > 0:
+        print("{0}  |||  {1}".format(x.strip(), y.strip()), file = aligned)
+aligned.close()
+
