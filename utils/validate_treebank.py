d2a3ba09b3f74c9167459e7c678c62e32b42b694
maryszmary <masha.shejanova@gmail.com>
2018-06-21 23:12:47 +0300

    updated validation script

--- utils/validate_treebank.py
+++ utils/validate_treebank.py
@@ -12,20 +12,33 @@ def validate(treebank):
 	doubled = []
 	missing = []
 	valid = []
+	wrong_order = []
+	wrong_start = []
+	too_large_head = []
 	for s in treebank:
 		ids = [int(t[0]) for t in s.tokens]
+		heads = [int(t[6]) for t in s.tokens]
 		if len(set(ids)) != len(ids):
 			doubled.append(s)
 			# print('doubled:\n')
 			# print(s)
+		elif ids[0] != 1:
+			wrong_start.append(s)
 		elif len(ids) < ids[-1]:
 			# print('missing:\n')
 			# print(s)
 			missing.append(s)
+		elif ids != list(sorted(ids)):
+			wrong_order.append(s)
+		elif max(heads) > max(ids):
+			too_large_head.append(s)
 		else:
 			valid.append(s)
 	print('doubled: ' + str(len(doubled)))
 	print('missing: ' + str(len(missing)))
+	print('wrong_order: ' + str(len(wrong_order)))
+	print('wrong_start: ' + str(len(wrong_start)))
+	print('too_large_head: ' + str(len(too_large_head)))
 	print('valid: ' + str(len(valid)))
 	return valid
 
@@ -38,7 +51,7 @@ def main():
 	# print(treebank)
 	if not os.path.exists('validated'):
 		os.mkdir('validated')
-	with open('validated/' + sys.argv[1].split('/')[-1], 'w') as f:
+	with open('validated/' + sys.argv[1].split('/')[-1], 'w', encoding='utf-8') as f:
 		f.write('\n\n'.join(str(s) for s in treebank))
 
 
