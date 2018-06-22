705669506a5c43f38721e526a67379518d8bc743
maryszmary <masha.shejanova@gmail.com>
2018-06-22 22:17:45 +0300

    a fast function for writing the union's sentences with 3 ar 4 variants

--- utils/treebanks_union.py
+++ utils/treebanks_union.py
@@ -18,16 +18,8 @@ def treebanks_dict():
 			# where n is a number of treebank versions
 
 			whole = one_treebank_dict(sents, whole)
-	print(len(whole))
-	trnum = 0
-	tnum = 0
-	for num in whole:
-		if len(whole[num]) == 3:
-			trnum += 1
-		elif len(whole[num]) == 2:
-			tnum += 1
-	print(trnum)
-	print(tnum)
+	print('uniuon: ' + str(len(whole)))
+	return whole
 
 
 def one_treebank_dict(sents, whole):
@@ -51,5 +43,36 @@ def unite_treebanks(tbs):
 	# treebank.append([Sentence(s) for s in sents])
 
 
+def fast_write_3_4(whole):
+	three_sents = [[], [], []]
+	four_sents = [[], [], [], []]
+	for num in whole:
+		sents = whole[num]
+		if len(sents) == 3:
+			three_sents[0].append(str(sents[0]))
+			three_sents[1].append(str(sents[1]))
+			three_sents[2].append(str(sents[2]))
+		elif len(sents) == 4:
+			four_sents[0].append(str(sents[0]))
+			four_sents[1].append(str(sents[1]))
+			four_sents[2].append(str(sents[2]))
+			four_sents[3].append(str(sents[3]))
+	with open('tmp/three_1st.conllu', 'w') as f:
+		f.write('\n\n'.join(three_sents[0]))
+	with open('tmp/three_2nd.conllu', 'w') as f:
+		f.write('\n\n'.join(three_sents[1]))
+	with open('tmp/three_3rd.conllu', 'w') as f:
+		f.write('\n\n'.join(three_sents[2]))
+	with open('tmp/four_1st.conllu', 'w') as f:
+		f.write('\n\n'.join(four_sents[0]))
+	with open('tmp/four_2nd.conllu', 'w') as f:
+		f.write('\n\n'.join(four_sents[1]))
+	with open('tmp/four_3rd.conllu', 'w') as f:
+		f.write('\n\n'.join(four_sents[2]))
+	with open('tmp/four_4th.conllu', 'w') as f:
+		f.write('\n\n'.join(four_sents[3]))
+
+
 if __name__ == '__main__':
-	treebanks_dict()
+	union = treebanks_dict()
+	# fast_write_3_4(union)
